8d781c5a6d63baa4ad2e69d4bed886933b36ae74
Luca Mosti <luca.mosti97@gmail.com>
2020-11-06 09:22:48 +0100

    Arbeitsauftrag Intercepting Save Data'

---
+++ 8_Intercepting/saveData/sw.js
@@ -0,0 +1,56 @@
+var cacheName = 'latestNews-v1';
+
+// Cache our known resources during install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll([
+      './js/main.js',
+      './js/article.js',
+      './images/newspaper.svg',
+      './css/site.css',
+      './data/latest.json',
+      './data/data-1.json',
+      './article.html',
+      './index.html'
+    ]))
+  );
+});
+
+//Save-Data
+this.addEventListener('fetch', function(event){
+  if(event.request.headers.get('save-data')){
+    if(event.request.url.includes('fonts.googleapis.com')){
+      event.respondWith(new Response('', {status: 417, statusText: 'Ignore fonts to save data'}));
+    }
+  }
+});
+
+// Cache any new resources as they are fetched
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true })
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function(response) {
+          if(!response || response.status !== 200) {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(cacheName)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        }
+      );
+    })
+  );
+});
